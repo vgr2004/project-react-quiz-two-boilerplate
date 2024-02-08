c5a4ba6acc8569ac5fdc4e41594d9eb047a42654
vignesh gr <141708172+vgr2004@users.noreply.github.com>
2024-02-08 13:19:32 +0530

    Update App.jsx

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,7 +1,5 @@
 import { Component } from "react";
-import HomeComp from "./components/HomeComp"
- import QuizComp from "./components/QuizComp"
- import ResultComp from "./components/ResultComp"
+import QuizComp from "./components/QuizComp";
 
 export default class App extends Component{
   render(){
